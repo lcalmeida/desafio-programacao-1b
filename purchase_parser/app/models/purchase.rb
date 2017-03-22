202932444af92009ad6af1e827394637810bbdc8
lcamargo <lucascamargodealmeida@gmail.com>
2017-03-21 23:38:16 -0300

    calculating gross_revenue

--- purchase_parser/app/models/purchase.rb
+++ purchase_parser/app/models/purchase.rb
@@ -1,2 +1,7 @@
 class Purchase < ActiveRecord::Base
+  def self.gross_revenue_of(purchases)
+    purchases.inject(0) do |sum, purchase|
+      sum + purchase.amount * purchase.unit_price
+    end
+  end
 end
